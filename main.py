67234491661f276e8e93b18fee9d1c430cd4b9de
fellipealvess <104930883+fellipealvess@users.noreply.github.com>
2022-06-27 16:33:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+import os
+from pygame.locals import*
+pygame.init
+pygame.font.init()
+pygame.mixer.init()
+
+tela_largura = 900
+tela_altura = 500
+largura_nave = 60
+altura_nave = 42
+
+colisao = pygame.mixer.sound(os.path.join('sons', 'col.mp3'))
+disparo = pygame.mixer.sound(os.path.join('sons', 'disparo.mp3'))
+fundo = pygame.image.load('imgs/fundodojogo.jpg')
+player1 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'player1.png')),
+                                          (largura_nave, altura_nave)), 90)
+
+
+tela = pygame.display.set_mode((tela_largura, tela_altura))
+pygame.display.set_caption(" ??? ")
+fps = pygame.time.Clock()
+
+
+
+
+jogo_aberto = True
+while jogo_aberto:
+    fps.tick(60)
+    tela.blit(fundo, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogo_aberto = False
+    tela.blit(player1, (100, 50))
+
+
+
+
+    pygame.draw.rect(tela,(0, 0, 0), ((tela_largura//2 - 5), 0, 10, tela_altura))
+    pygame.display.update()
+pygame.quit()
+
